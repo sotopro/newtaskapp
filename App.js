9f637aa24c90616e8493de39f2857fa0e6522662
sotopro <dasg.1993@gmail.com>
2023-01-11 19:12:23 -0500

    [feature] - add states to manage task and task list & create item component and styles

--- App.js
+++ App.js
@@ -1,12 +1,50 @@
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [task, setTask] = useState('');
+  const [tasks, setTasks] = useState([]);
+
+  const onHandlerChange = (text) => {
+    setTask(text)
+  }
+
+  const onHandlerSubmit = () => {
+    setTasks([
+      ...tasks,
+      {
+        id: Math.random().toString(),
+        value: task
+      }
+    ]);
+    setTask('');
+  }
+
+  console.warn('tasks', tasks)
+
   return (
     <View style={styles.container}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.input} placeholder='add a new task' />
-        <Button title='Add' color='#626893' />
+        <TextInput 
+          style={styles.input} 
+          placeholder='add a new task' 
+          autoComplete='off'
+          autoCorrect={false}
+          autoCapitalize='none'
+          value={task}
+          onChangeText={onHandlerChange}
+        />
+        <Button disabled={!task} title='Add' color='#626893' onPress={onHandlerSubmit} />
       </View>
+      <View style={styles.listContainer}>
+          {
+            tasks.map((item) => (
+              <View style={styles.itemContainer}>
+              <Text style={styles.itemList} key={item.id}>{item.value}</Text>
+              </View>
+            ))
+          }
+        </View>
     </View>
   );
 }
@@ -17,13 +55,35 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
   },
   inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     marginTop: 50,
     marginHorizontal: 20,
   },
   input: {
+    width: '75%',
     borderBottomColor: '#626893',
     borderBottomWidth: 1,
     height: 40,
     color: '#212121'
+  },
+  listContainer: {
+    marginHorizontal: 20,
+    marginTop: 40
+  },
+  itemContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    backgroundColor: '#626893',
+    marginBottom: 10,
+    borderRadius: 10,
+  },
+  itemList: {
+    fontSize: 14,
+    color: '#ffffff',
+    fontWeight: 'bold'
   }
 });
